113bc3a7e919b7496a9f0ba441cd953e7d028e12
aditya-shankaran-acc <89574978+aditya-shankaran-acc@users.noreply.github.com>
2021-09-20 18:28:17 +0530

    qqqq

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,1 +1,20 @@
 import React, { Component } from 'react'
+
+class QuizQuestion extends Component{
+    render(){
+        return(
+            <main>
+                <section>
+                        <p>// instruction text goes here</p>
+                </section>
+                <section className="buttons">
+                    <ul>
+                        //button logic
+                    </ul>
+                </section>
+            </main>
+        )
+    }
+}
+
+export default QuizQuestion
