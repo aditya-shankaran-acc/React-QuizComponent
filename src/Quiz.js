5c87257b538b9d02ebef3b347d9e7a4cc2a01788
aditya-shankaran-acc <89574978+aditya-shankaran-acc@users.noreply.github.com>
2021-09-20 18:02:28 +0530

    Quiz

---
+++ src/Quiz.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'reacteact'
+
+let QuizData=require('./quiz_data.json')
+
+class Quiz extends Component{
+  constructor(props){
+    super(props)
+    this.state={quiz_position: 1}
+  }
+  render(){
+    return(
+      <div>
+        <div className="QuizQuestion">{quizData.quiz_questions[0].instructions_text}</div>
+      </div>
+    )
+  }
+}
+
+export default Quiz
